6cb19fd8f12188c95bdc772425d706e222301baa
izuichi <polarbrou0103@gmail.com>
2026-09-24 08:20:45 -0500

    asesino-magico

--- routers/personajes.py
+++ routers/personajes.py
@@ -23,6 +23,14 @@ UNIVERSO = [
         "clase": "Hechicero de las bases de datos",
         "habilidad_especial": "Hace inner joins con un solo hechizo",
         "nivel": 100
+    },
+    {
+        "creador": "Jeffry duarte",
+        "nombre": "izuichi",
+        "clase": "asesino-magico",
+        "habilidad_especial": "torcion testicular",
+        "nivel": 100
+
     }
 ]
 
